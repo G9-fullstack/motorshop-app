6ade1b7891ab2b69cbda04da1a816d63c13b2a3c
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2023-06-09 17:02:14 -0300

    feat: component filter and carCard created

--- src/app/announcement/page.tsx
+++ src/app/announcement/page.tsx
@@ -1,5 +1,11 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 export default function Announcement() {
   return (
-    <main></main>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
---
+++ src/components/Filter.tsx
@@ -0,0 +1,33 @@
+export default function Filter() {
+  return (
+    <div className="flex flex-col w-64 p-4 mt-12 absolute top-38 left-0">
+      <h2 className="font-semibold text-lg">Marca</h2>
+      <p className="text-brand-brand3 px-2">Porsche</p>
+      <h2 className="font-semibold text-lg">Modelo</h2>
+      <p className="text-brand-brand3 px-2">Porsche</p>
+      <h2 className="font-semibold text-lg"> Cor</h2>
+      <p className="text-brand-brand3 px-2">Azul</p>
+      <p className="text-brand-brand3 px-2">Branca</p>
+      <p className="text-brand-brand3 px-2">Prata</p>
+      <p className="text-brand-brand3 px-2">Cinza</p>
+      <p className="text-brand-brand3 px-2">Verde</p>
+      <h2 className="font-semibold text-lg py-1">Ano</h2>
+      <p className="text-brand-brand3 px-2">2012</p>
+      <p className="text-brand-brand3 px-2">2021</p>
+      <p className="text-brand-brand3 px-2">2022</p>
+      <p className="text-brand-brand3 px-2">2014</p>
+      <p className="text-brand-brand3 px-2">2018</p>
+      <h2 className="font-semibold text-lg">Combustivel</h2>
+      <p className="text-brand-brand3 px-2">Diesel</p>
+      <p className="text-brand-brand3 px-2">Etanol</p>
+      <p className="text-brand-brand3 px-2">Gasolina</p>
+      <p className="text-brand-brand3 px-2">Flex</p>
+      <h2 className="font-semibold text-lg">Km</h2>
+      <p className="text-brand-brand3 px-2">Minimo</p>
+      <p className="text-brand-brand3 px-2">Maximo</p>
+      <h2 className="font-semibold text-lg">Preço</h2>
+      <p className="text-brand-brand3 px-2">minimo</p>
+      <p className="text-brand-brand3 px-2">maximo</p>
+    </div>
+  );
+}

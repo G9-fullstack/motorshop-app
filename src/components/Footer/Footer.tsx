0c7d1c8f66a0c41aa687867c240ed53fbafc33bc
Gabriel Pavão <gabpavao@outlook.com>
2023-06-09 09:00:23 -0300

    feat: Footer component basic HTML

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { ChevronUp } from 'lucide-react';
+
+export default function Footer() {
+	return (
+		<footer>
+			<h1>Motors <span>shop</span></h1>
+			<p>© 2022 - Todos os direitos reservados.</p>
+			<button>
+				<ChevronUp/>
+			</button>
+		</footer>
+	)
+}

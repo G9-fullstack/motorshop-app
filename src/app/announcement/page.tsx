eba912145164b92408d7f69b346a5ab96b630daa
Gabriel Pavão <gabpavao@outlook.com>
2023-06-09 17:46:57 -0300

    feat: mount components and fix responsive styles

--- src/app/announcement/page.tsx
+++ src/app/announcement/page.tsx
@@ -1,9 +1,21 @@
 import AnnounceDetail from '@/components/AnnounceDetail'
+import CardUserProfile from '@/components/CardUserProfile'
+import CommentForm from '@/components/CommentForm'
+import Comments from '@/components/Comments'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 export default function Announcement() {
   return (
-	<main>
-		<AnnounceDetail/>
-	</main>
+	<>
+		<Header/>
+		<main>
+			<AnnounceDetail/>
+			<CardUserProfile/>
+			<Comments/>
+			<CommentForm/>
+		</main>
+		<Footer/>
+	</>
   )
 }

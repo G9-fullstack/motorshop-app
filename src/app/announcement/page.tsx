0e8679a42537c3defa70398db12abc517003177e
vitor-galdino <httpvittor@gmail.com>
2023-06-10 00:56:37 -0300

    feat: adapt responsive desing to desktop screens

--- src/app/announcement/page.tsx
+++ src/app/announcement/page.tsx
@@ -1,15 +1,14 @@
 import AnnounceDetail from '@/components/AnnounceDetail';
-import CardUserProfile from '@/components/CardUserProfile';
-import CommentForm from '@/components/CommentForm';
-import Comments from '@/components/Comments';
 
 export default function Announcement() {
 	return (
-		<main>
-			<AnnounceDetail />
-			<CardUserProfile />
-			<Comments />
-			<CommentForm />
-		</main>
+		<>
+			<div className='bg-grey-grey8 pb-16'>
+				<div className='absolute z-0 top-20 bg-brand-brand1 w-full h-[442px] md:h-[582px]' />
+				<main className='container m-auto relative z-10 px-3'>
+					<AnnounceDetail />
+				</main>
+			</div>
+		</>
 	);
 }

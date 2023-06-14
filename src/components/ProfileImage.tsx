6f25d7240da9402f7def707d845c7912fb6196ae
vitor-galdino <httpvittor@gmail.com>
2023-06-14 00:37:15 -0300

    feat: add size funcionality

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -2,26 +2,27 @@
 
 interface ProfileImageProps {
   name: string;
+  size: "small" | "big"
 }
 
 function getInitials(name: string) {
   const names = name.split(" ");
-  if (names.length > 1) {
+  if (names.length > 1 && names[1].length > 1) {
     return names[0][0] + names[1][0];
   } else {
     return names[0][0];
   }
 }
 
-
-export default function ProfileImage({ name, }: ProfileImageProps) {
+export default function ProfileImage({ name, size, }: ProfileImageProps) {
   const initials = getInitials(name);
 
   return (
-    <div className={
-      "w-[104px] h-[104px] bg-random-profile-11 rounded-full flex justify-center items-center font-inter font-medium text-4xl text-grey-whiteFixed"
-    }>
+    <span className={`
+      ${size == "big" ? "xl:w-[6.5rem] xl:h-[6.5rem] xl:text-4xl text-3xl w-[4.8125rem] h-[4.8125rem]" : "w-8 h-8 text-sm"} 
+       rounded-full flex justify-center items-center font-inter font-medium text-grey-whiteFixed
+    `}>
       {initials}
-    </div>
+    </span>
   );
 }

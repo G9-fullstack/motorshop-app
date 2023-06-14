1e639574ebdc0cbd3f07306d58bce6b3ba793d69
vitor-galdino <httpvittor@gmail.com>
2023-06-14 17:38:26 -0300

    feat: add user field on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,24 @@
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { use, useState } from "react";
 import brandingLogo from "../../public/branding-logo.svg";
 import { usePathname } from "next/navigation";
+import ProfileImage from "./ProfileImage";
 
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const isLogged = true;
   const pathname = usePathname();
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 grid h-20 px-14 border-b-2 bg-grey-whiteFixed place-items-center border-grey-6">
+    <header className="fixed top-0 left-0 right-0 z-50 grid h-20 border-b-2 px-14 bg-grey-whiteFixed place-items-center border-grey-6">
       <div className="container flex items-center justify-between h-full">
         <Image src={brandingLogo} alt="Motorshop logo" height={26} />
-        <div className="hidden md:flex md:items-center">
-          {pathname != "/" && (<Link href={"/"} className="text-grey-2 font-inter font-normal text-base mr-11">Home</Link>)}
+        {!isLogged && (<div className="hidden h-full md:flex md:items-center">
+          {pathname != "/" && (<Link href={"/"} className="text-base font-normal text-grey-2 font-inter mr-11">Home</Link>)}
           <div className="flex items-center h-full border-l-2 border-grey-6 font-inter pl-11 gap-11">
             <Link href={"/login"} className="text-base font-semibold leading-7 text-grey-2">
               Fazer Login
@@ -25,7 +28,32 @@ export default function Header() {
               Cadastrar
             </Link>
           </div>
-        </div>
+        </div>)}
+        {isLogged && (<div className="relative flex items-center h-full border-l-2 border-grey-6 pl-11">
+          <button
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            className="hidden md:flex items-center gap-2"
+          >
+            <ProfileImage name="Samuel Leão" size="small" userId={10} />
+            <span className="text-base font-normal font-inter text-grey-2">Samuel Leão</span>
+          </button>
+          <div className="absolute top-full right- z-[9999] w-[12.5rem] bg-grey-9 rounded shadow-2xl">
+            <ul>
+              <li>
+                <button>Editar Perfil</button>
+              </li>
+              <li>
+                <button>Editar endereço</button>
+              </li>
+              <li>
+                <button>Meus Anúncios</button>
+              </li>
+              <li>
+                <button>Sair</button>
+              </li>
+            </ul>
+          </div>
+        </div>)}
         <div
           className="cursor-pointer md:hidden text-grey-1"
           onClick={() => setIsOpen(!isOpen)}
@@ -33,11 +61,11 @@ export default function Header() {
           {isOpen ? <X /> : <Menu />}
         </div>
       </div>
-      {isOpen && (<div className="absolute flex flex-col justify-between w-full px-3 py-8 gap-4 top-full h-max bg-grey-whiteFixed md:hidden">
+      {!isLogged && isOpen && (<div className="absolute flex flex-col justify-between w-full gap-4 px-3 py-8 shadow-2xl top-full h-max bg-grey-whiteFixed md:hidden">
         {pathname != "/login" && pathname != "/register" && (<Link href={"/login"} className="text-base font-semibold leading-7 text-grey-2">
           Fazer Login
         </Link>)}
-        {pathname != "/" && (<Link href={"/"} className="text-base font-semibold leading-7  text-grey-2">
+        {pathname != "/" && (<Link href={"/"} className="text-base font-semibold leading-7 text-grey-2">
           Ir para Home
         </Link>)}
         <Link href={pathname == "/register" ? "/login" : "/register"} className="px-5 py-3 text-base font-semibold text-center bg-transparent border-2 rounded border-grey-4 text-grey-0">

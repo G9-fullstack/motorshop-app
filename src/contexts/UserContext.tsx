4e53b0aeb0e0533f695c419dd3100b891a46fbbd
vitor-galdino <httpvittor@gmail.com>
2023-06-16 17:52:13 -0300

    feat: add username state and receive username after request

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -2,7 +2,7 @@
 import { userData } from "@/schemas/user.schema";
 import api from "@/services/api";
 import { useRouter } from "next/navigation";
-import { ReactNode, createContext, useContext } from "react";
+import { ReactNode, createContext, useContext, useState } from "react";
 
 interface Props {
   children: ReactNode;
@@ -11,12 +11,16 @@ interface Props {
 interface UserContextData {
   handleUserCreate: (formData: userData) => void;
   handleUserLogin: (formData: userData) => void;
+  username: string;
+  setUsername: React.Dispatch<React.SetStateAction<string>>
 }
 
 const UserContext = createContext({} as UserContextData);
 
 export function UserProvider({ children, }: Props) {
+  const [username, setUsername] = useState<string>("");
   const router = useRouter();
+
   function handleUserCreate(formData: userData) {
     api
       .post("/users", formData)
@@ -31,7 +35,8 @@ export function UserProvider({ children, }: Props) {
   function handleUserLogin(formData: userData) {
     api
       .post("/login", formData)
-      .then(() => {
+      .then(({data,}) => {
+        setUsername(data.user.name);
         router.push("/");
       })
       .catch((err) => {
@@ -44,6 +49,8 @@ export function UserProvider({ children, }: Props) {
       value={{
         handleUserCreate,
         handleUserLogin,
+        setUsername,
+        username,
       }}
     >
       {children}

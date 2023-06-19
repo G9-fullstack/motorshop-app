12b7659914a44351c8e73af49007bc87819734dd
vitor-galdino <httpvittor@gmail.com>
2023-06-19 11:36:18 -0300

    style: adjust modal height and weigth

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -20,8 +20,8 @@ export function Modal(props: ModalProps): JSX.Element | null {
   }, [modalElement, modalRoot]);
 
   return props.isOpen ? createPortal(
-    <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur-sm h-screen">
-      <section className="bg-grey-10 rounded py-11 px-12 w-[25.6875rem] mt-40 mb-40 h-[90vh] overflow-y-auto scrollbar-thin scrollbar-thumb-branding-blue scrollbar-thumb-rounded-full">
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/50 h-screen">
+      <section className="bg-grey-10 rounded py-11 px-12 w-[32.5rem] mt-40 mb-40 max-h-[90vh] overflow-y-auto scrollbar-thin scrollbar-thumb-branding-blue scrollbar-thumb-rounded-full">
         <div className="flex justify-between">
           <h2 className="mb-4 text-1 font-medium text-black font-lexend">{props.modalTitle}</h2>
           <button

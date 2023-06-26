5dc593d080d72894d44be0dd5b1f2626ffc53b6c
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2023-06-26 17:34:32 -0300

    feat: toasts apllied

--- src/components/EditAddressForm.tsx
+++ src/components/EditAddressForm.tsx
@@ -5,14 +5,15 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useUser } from "@/contexts/UserContext";
 import { useEffect } from "react";
 import { addressData, addressSchema } from "@/schemas/address.schema";
+import { Toaster, toast } from "sonner";
 
 interface iEditAddressFormProps {
-	closeModal: () => void;
+  closeModal: () => void;
 }
 
-export default function EditAddressForm({ closeModal, }: iEditAddressFormProps) {
-  const { handleEditAddress, handleRetrieveUserAddress, } = useUser();
-  const { register, handleSubmit, setValue, } = useForm<addressData>({
+export default function EditAddressForm({ closeModal }: iEditAddressFormProps) {
+  const { handleEditAddress, handleRetrieveUserAddress } = useUser();
+  const { register, handleSubmit, setValue } = useForm<addressData>({
     mode: "onSubmit",
     resolver: zodResolver(addressSchema),
   });
@@ -22,12 +23,14 @@ export default function EditAddressForm({ closeModal, }: iEditAddressFormProps) 
       ...data,
       zipCode: data.zipCode.replace(/[^0-9]/g, ""),
     });
+    toast.success("Endereço alterado com sucesso");
     closeModal();
   }
 
   useEffect(() => {
     async function handleRetrieve() {
-      const { zipCode, state, city, street, number, complement, } = await handleRetrieveUserAddress();
+      const { zipCode, state, city, street, number, complement } =
+        await handleRetrieveUserAddress();
 
       setValue("zipCode", zipCode.slice(0, 5) + "-" + zipCode.slice(5));
       setValue("state", state);
@@ -44,39 +47,74 @@ export default function EditAddressForm({ closeModal, }: iEditAddressFormProps) 
       <fieldset className="space-y-5">
         <legend>Informações pessoais</legend>
 
-        <Input label="CEP" type="text" name="editZipCode" register={register("zipCode")}/>
+        <Input
+          label="CEP"
+          type="text"
+          name="editZipCode"
+          register={register("zipCode")}
+        />
 
         <div className="flex gap-3">
           <fieldset className="flex flex-col gap-y-2">
             <label htmlFor="editState">Estado</label>
-            <input type="text" id="editState" className="px-4 py-2 text-grey-1 rounded w-full border-2 border-grey-7 input-placeholder hover:border-grey-8 focus:border-brand-1" {...register("state")}/>
+            <input
+              type="text"
+              id="editState"
+              className="px-4 py-2 text-grey-1 rounded w-full border-2 border-grey-7 input-placeholder hover:border-grey-8 focus:border-brand-1"
+              {...register("state")}
+            />
           </fieldset>
 
           <fieldset className="flex flex-col gap-y-2">
             <label htmlFor="editCity">Cidade</label>
-            <input type="text" id="editCity" className="px-4 py-2 text-grey-1 rounded w-full border-2 border-grey-7 input-placeholder hover:border-grey-8 focus:border-brand-1" {...register("city")}/>
+            <input
+              type="text"
+              id="editCity"
+              className="px-4 py-2 text-grey-1 rounded w-full border-2 border-grey-7 input-placeholder hover:border-grey-8 focus:border-brand-1"
+              {...register("city")}
+            />
           </fieldset>
         </div>
 
-        <Input label="Rua" type="text" name="editStreet" register={register("street")}/>
+        <Input
+          label="Rua"
+          type="text"
+          name="editStreet"
+          register={register("street")}
+        />
 
         <div className="flex gap-3">
           <fieldset className="flex flex-col gap-y-2">
             <label htmlFor="number">Número</label>
-            <input type="text" id="number" className="px-4 py-2 text-grey-1 rounded w-full border-2 border-grey-7 input-placeholder hover:border-grey-8 focus:border-brand-1" {...register("number")}/>
+            <input
+              type="text"
+              id="number"
+              className="px-4 py-2 text-grey-1 rounded w-full border-2 border-grey-7 input-placeholder hover:border-grey-8 focus:border-brand-1"
+              {...register("number")}
+            />
           </fieldset>
 
           <fieldset className="flex flex-col gap-y-2">
             <label htmlFor="editComplement">Complemento</label>
-            <input type="text" id="editComplement" className="px-4 py-2 text-grey-1 rounded w-full border-2 border-grey-7 input-placeholder hover:border-grey-8 focus:border-brand-1" {...register("complement")}/>
+            <input
+              type="text"
+              id="editComplement"
+              className="px-4 py-2 text-grey-1 rounded w-full border-2 border-grey-7 input-placeholder hover:border-grey-8 focus:border-brand-1"
+              {...register("complement")}
+            />
           </fieldset>
         </div>
 
         <div className="flex justify-end gap-3">
-          <Button onClick={closeModal} type="button" size="big" style="grey-2">Cancelar</Button>
-          <Button type="submit" size="big" style="brand-3">Salvar Alterações</Button>
+          <Button onClick={closeModal} type="button" size="big" style="grey-2">
+            Cancelar
+          </Button>
+          <Button type="submit" size="big" style="brand-3">
+            Salvar Alterações
+          </Button>
         </div>
       </fieldset>
+      <Toaster position="top-center" expand={true} />
     </form>
   );
 }
